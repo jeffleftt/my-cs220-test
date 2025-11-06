8cac71dfa4b8b83ebe2db60214286a2ae2b804bb
jhuallpa <jhuallpa@student.cccd.edu>
2025-11-06 13:05:28 -0800

    string stuff

---
+++ README.md
@@ -0,0 +1,17 @@
+# this is a level 1 heading 
+
+This is my cs220 test.
+
+## A Level 2 heading 
+Favorite 
+* item 1
+* chocolate
+* vanilla
+
+
+## B Level 2 heading 
+
+Ordered List 
+1. abc
+2. def 
+3. ghi 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 // hi 
 #include <iostream> 
+#include <string>
 using namespace std;
 int main() {
     cout << "Hello World"; endl;
     cout << "This is fun!"; << endl;
+
+
+    string s = "abc";
+    cout << s << endl;
 }
